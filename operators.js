9b3acc9922ad3095254af932fa195d8c3b17c413
iyoushhhhh_ <iyoushhhhh007@users.noreply.github.com>
2022-11-22 01:19:21 +0530

    new file:   operators.js

---
+++ operators.js
@@ -0,0 +1,27 @@
+console.log("Operators in Js")
+let a = 4
+let b = 6
+console.log(" ")
+
+
+console.log("a + b = ", a+b)
+console.log("a - b = ", a-b)
+console.log("a * b = ", a*b)
+console.log("a / b = ", a/b)
+console.log("a ** b = ", a**b)
+console.log("a % b = ", a%b)
+console.log(" ")
+
+
+//Increment Operator
+console.log("a++ = ", a++)
+console.log("a = ", a)
+console.log("++a = ", ++a)
+console.log(" ") 
+
+//Decrement Operator
+console.log("b-- = ", b--)
+console.log("b = ", b)
+console.log("--b = ", --b)
+
+
