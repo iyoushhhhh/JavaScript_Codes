fd3e315420d8a9404e9f3986af68724364294e60
iyoushhhhh_ <iyoushhhhh007@users.noreply.github.com>
2022-11-22 01:32:10 +0530

    modified:   operators.js

--- operators.js
+++ operators.js
@@ -24,4 +24,28 @@ console.log("b-- = ", b--)
 console.log("b = ", b)
 console.log("--b = ", --b)
 
+//Comparison Operator
+let x = 5
+let y = 8
+console.log("x == y : ", x == y)
+
+let num1 = 7
+let num2 = "7"
+console.log("num1 == num2", num1 == num2)
+console.log("num1 != num2", num1 != num2)
+console.log("num1 === num2", num1 === num2)
+console.log("num1 !== num2", num1 !== num2)
+
+//Logical Operators
+let num = 8
+let num_1 = 9
+
+console.log( num < num_1 && num == 8)
+console.log( num < num_1 || num == 9)
+
+
+
+
+
+
 
