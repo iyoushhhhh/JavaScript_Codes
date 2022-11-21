45225664102bfe94853cda7997ad3ab88e42ea31
iyoushhhhh_ <iyoushhhhh007@users.noreply.github.com>
2022-11-21 18:11:18 +0530

    new file:   variables.js 	deleted:    xyz.js

---
+++ variables.js
@@ -0,0 +1,47 @@
+// var a = 89;
+// console.log(a);
+
+// let x = 99;
+// console.log(x);
+
+// a = "Ayush Gautam";
+// console.log(a);
+
+//"VAR" was pre *ES6*
+//"LET" is used to define BlockScope Variables
+//"Let" can be updated but no re-declared.
+
+// var a = "Ayush";
+// {
+//   var a = "Gautam";
+//   console.log(a);
+// }
+// console.log(a);
+
+// //With "Let"
+// let x = "Ashar";
+// {
+//   let x = "hasan";
+//   console.log(x);
+// }
+// console.log(x);
+
+// const name = "JK Rowling";
+
+//JavaScript Code to Store name, phone number and marks of a student
+
+console.log("Student Data");
+
+let name = "Ayush Gautam";
+const Phone = 7903261994;
+let marks = 89;
+
+let name2 = "Ashar Hasan";
+const Phone2 = 7903261997;
+let marks2 = 67;
+
+console.log(name, Phone, marks);
+console.log(name2, Phone2, marks2);
+
+
+
