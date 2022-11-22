b5cc56fc4460ddf74805e9f2046494d874105bea
iyoushhhhh_ <iyoushhhhh007@users.noreply.github.com>
2022-11-22 17:36:27 +0530

    modified:   conditional.js

--- conditional.js
+++ conditional.js
@@ -1,13 +1,29 @@
 //Conditional Statements in JS
 
-let name = prompt("Enter you name: ")
-let age  = prompt("Enter your age: ")
+// let name = prompt("Enter you name: ")
+// let age  = prompt("Enter your age: ")
 
-age = Number.parseInt(age) //To Convert String into Integer Type
-// console.log(typeof age)
+// age = Number.parseInt(age) //To Convert String into Integer Type
+// // console.log(typeof age)
 
-if(age < 18 ){
-    alert("Hello "+name +", you are under-age");
-}else{
-    alert("You are within age limit.")
+
+// if(age < 18 ){
+//     alert("Hello "+name +", you are under-age");
+// }else{
+//     alert("You are within age limit.")
+// }
+
+//Switch Statement
+
+const object = "pen"
+
+switch( object ) {
+    case 'pen' : 
+    console.log(object + " is used for writing")
+    break;
+    case 'bat' :
+    console.log(object +" is used for playing cricket")
+    break;
+    default:
+        console.log(object + " is not available.")
 }
