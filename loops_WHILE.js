7392132fa4bcacbde5af9525e8c9c3c78dd91800
iyoushhhhh_ <iyoushhhhh007@users.noreply.github.com>
2022-11-30 00:30:52 +0530

    modified:   loops_WHILE.js

--- loops_WHILE.js
+++ loops_WHILE.js
@@ -3,3 +3,14 @@ while( value < 10 ){
     console.log(value)
     value++;
 }
+
+//Do While Loop
+
+//Block is executed atleast one time before checking the condition.
+let count = 0
+let i = 0;
+do{
+    console.log('Code executed '+i+" time")
+    i++
+    count++
+}while( count < 10)
