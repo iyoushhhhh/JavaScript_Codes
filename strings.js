6e0a11de9a7517da21ca9710b1cfe1e66f2a1773
iyoushhhhh_ <iyoushhhhh007@users.noreply.github.com>
2022-11-30 11:56:57 +0530

    new file:   strings.js

---
+++ strings.js
@@ -0,0 +1,26 @@
+//Strings are used to store and manipulate text. 
+let name = 'Ayush'
+let name1 = "gautam"
+console.log(name + name1)
+
+console.log(name[0])
+console.log(name1[4])
+
+
+//********Template Liberals**************
+ let firstname = "Ayush Gautam"
+ let secondname = "Rahul"
+
+//We can insert variables directly in template literal. This is called "string interpolation".
+ let sentence = `${firstname} is a friend of ${secondname}`
+ console.log(sentence)
+
+
+ //******Escape sequence characters*********
+
+ let obj = "G\(old\)" //G(old)
+ console.log(obj)
+
+ // \n -> new line
+ // \t -> Tab
+ // \r -> Carriage Return
