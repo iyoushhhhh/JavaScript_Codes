929876861ea3b4f4be5576b7aa27e0987dcc2462
Ayush Gautam <76200266+iyoushhhhh@users.noreply.github.com>
2022-12-06 00:13:10 +0530

    Add files via upload

---
+++ Console Object/console_object.js
@@ -0,0 +1,15 @@
+console.log("Check.")
+
+//Console object methods
+
+// assert()  -> used to assert a condition
+// clear ( ) -> clears the console
+// log ( )   -> Outputs a message to the console
+
+// table ( ) -> Displays a tabular data
+// obj = {a : 23, b : 24, c : 25 }
+// console.table(obj)
+
+// warn( )   -> used for warnings
+// error ( ) -> used for errors
+// info( )   -> used for special information
