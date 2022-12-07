09556064211c93a5a5930f21e1578aa47d7991c3
Ayush Gautam <76200266+iyoushhhhh@users.noreply.github.com>
2022-12-07 23:13:15 +0530

    Add files via upload

---
+++ DOM BOM & Window Object/script.js
@@ -0,0 +1,7 @@
+//Window Object represents browser window and provides methods to control it.
+console.log(window)
+
+//Document Object Model : DOM represents the page content as HTML.
+
+console.log(document)
+document.body.style.background = "red"
