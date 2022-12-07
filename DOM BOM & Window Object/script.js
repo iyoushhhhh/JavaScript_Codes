d0b31eb2f8ba79e8d5bf767712e91503bd002673
Ayush Gautam <76200266+iyoushhhhh@users.noreply.github.com>
2022-12-07 23:22:41 +0530

    Update script.js

--- DOM BOM & Window Object/script.js
+++ DOM BOM & Window Object/script.js
@@ -4,4 +4,9 @@ console.log(window)
 //Document Object Model : DOM represents the page content as HTML.
 
 console.log(document)
-document.body.style.background = "red"
+let colour = prompt("enter the background colour: ")
+document.body.style.background = colour
+
+//Browser Object Model : The Browser Object Model (BOM) represents additional objects provided by the browser for working with everything except the document. "alert / confirm / prompt are also a part of the BOM"
+
+//location.href = "https://www.google.com"
