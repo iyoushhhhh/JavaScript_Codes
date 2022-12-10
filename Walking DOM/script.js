ca7bd1e6ee8a525de3c3114193301af668faf643
Ayush Gautam <76200266+iyoushhhhh@users.noreply.github.com>
2022-12-10 20:15:07 +0530

    Add files via upload

---
+++ Walking DOM/script.js
@@ -0,0 +1,4 @@
+// Auto Correction in DOM -> If an erroneous HTML is encountered by the browser. it tends to correct after the body, it is automatically moved inside the body. 
+
+console.log(document.title) //This returns string not title tag
+
