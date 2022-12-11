eb6f8137bebc88b459432dfddf1700d5d8b7f854
Ayush Gautam <76200266+iyoushhhhh@users.noreply.github.com>
2022-12-12 00:25:56 +0530

    Update script.js

--- Walking DOM/script.js
+++ Walking DOM/script.js
@@ -2,3 +2,6 @@
 
 console.log(document.title) //This returns string not title tag
 
+console.log(document.body.firstChild)
+console.log(document.body.lastChild)
+console.log(document.body.childNodes)
