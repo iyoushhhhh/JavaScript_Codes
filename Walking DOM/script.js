31befe5330d1d9a6823cef8b0c8440a7645a6a0d
Ayush Gautam <76200266+iyoushhhhh@users.noreply.github.com>
2022-12-12 00:36:58 +0530

    Update script.js

--- Walking DOM/script.js
+++ Walking DOM/script.js
@@ -2,6 +2,10 @@
 
 console.log(document.title) //This returns string not title tag
 
-console.log(document.body.firstChild)
+console.log(document.body.firstChild) // or body.child[0]
 console.log(document.body.lastChild)
-console.log(document.body.childNodes)
+// console.log(document.body.childNodes)
+
+let arr = Array.from(document.body.childNodes)
+console.log(arr)
+//ChildNodes looks like an array. But its not actually an array but a collection. We can use Array.from(collection) to convert it into Array.
